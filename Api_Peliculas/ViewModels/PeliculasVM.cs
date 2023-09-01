b977db2fc75f0d81d5b926f754b2a0676e1838b0
Haru Baldeon <40285629+htecnology@users.noreply.github.com>
2023-09-01 16:57:14 -0500

    Agregar ViewModels Peliculas

---
+++ Api_Peliculas/ViewModels/PeliculasVM.cs
@@ -0,0 +1,12 @@
+﻿namespace Api_Peliculas.ViewModels
+{
+    public class PeliculasVM
+    {
+        public int Id { set; get; }
+        public string Nombre{ set; get; }
+        public string Fecha_Estreno { set; get; }
+        public string Imagen { set; get; }
+        public string Tiempo_Duracion { set; get; }
+
+    }
+}
